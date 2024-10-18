068b26f4e547f69ecec0173a10fb665ada43e6a5
jeffers3n <jeffers3n@gmail.com>
2024-10-18 20:18:58 +0800

    Site updated: 2024-10-18 20:18:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/17/hello-world/","2023/10/10/财富/抄盘手05/"];function toRandomPost(){
+var posts=["2024/10/18/这是一篇新的博文/","2024/10/17/hello-world/","2023/10/10/财富/抄盘手05/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
